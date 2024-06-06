8aa2262f6e0493f9c5a70ebfa0b3ae8f507b0aba
DavidMejiaSo <davidmejiasoto97@gmail.com>
2024-06-05 23:18:16 -0500

    DEV:Entities and Mappers

---
+++ lib/infrastructure/entites/book.dart
@@ -0,0 +1,11 @@
+class Book {
+  final String cover_url; //Portrait cover
+  final String description; //Book description
+
+  final String title;
+
+  Book(
+      {required this.cover_url,
+      required this.description,
+      required this.title});
+}
---
+++ lib/infrastructure/entites/user.dart
@@ -0,0 +1,13 @@
+class User {
+  //Los Twwets
+  final String appKey;
+  final String oauth;
+  final String sesskey;
+  final String name;
+
+  User(
+      {required this.appKey,
+      required this.oauth,
+      required this.sesskey,
+      required this.name});
+}
